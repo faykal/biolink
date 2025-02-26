08fa6c2f042a34ad8163ef5082ccc25e21bea8ea
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-02-26 23:03:06 +0700

    Update Profile fix

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -33,7 +33,7 @@ const Profile = () => {
             <FaInstagram className="h-5 w-5 text-pink-600" />
           </a>
           <a href="https://www.tiktok.com/@faykalmlbb?_t=8qHhC1W8Ucy&_r=1" target="_blank" rel="noopener noreferrer" className="text-zinc-400 hover:text-zinc-100 transition-colors">
-            <FaTiktok className="h-5 w-5 text-white-600" />
+            <FaTiktok className="h-5 w-5 text-gray-600" />
           </a>
           <a href="https://wa.me/6285234754939" target="_blank" rel="noopener noreferrer" className="text-zinc-400 hover:text-zinc-100 transition-colors">
             <FaWhatsapp className="h-5 w-5 text-green-600" />
