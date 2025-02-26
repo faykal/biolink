b7df1cdb10b361c30af0b5eef5149b04b9d533f2
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-02-27 00:48:12 +0700

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,16 @@
-
-import React from "react";
-import { FaSpotify } from "react-icons/fa";
+import React from "react"
+import { PiStarFourLight } from "react-icons/pi"
 
 const Footer = () => {
-  return (
-    <footer className="text-center mt-6 text-sm text-gray-400">
-      <hr className="h-px bg-gray-800 my-4 border-0" />
-      <p>See what I'm currently listening on Spotify</p>
-      <div className="flex justify-center items-center gap-2 mt-1">
-        <FaSpotify className="text-green-500" />
-        <p>No listening to anything</p>
-      </div>
-    </footer>
-  );
-};
+    return (
+        <footer className="text-center mt-6 text-sm text-gray-400">
+            <hr className="h-px bg-gray-800 my-4 border-0"/>
+            <div className="flex justify-center items-center gap-2 mt-1">
+                <PiStarFourLight className="text-gray-500"/>
+                <p>© 2023 faykal.</p>
+            </div>
+        </footer>
+    )
+}
 
-export default Footer;
+export default Footer
