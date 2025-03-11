c1658cbe37277bb10d324c1fd95789adc716a149
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-03-11 19:22:16 +0700

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,15 @@
 jsx
 import React from "react";
-import { FaSpotify } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="text-center mt-6 text-sm text-gray-400">
       <hr className="h-px bg-gray-800 my-4 border-0" />
-      <p>See what I'm currently listening on Spotify</p>
+      <p>See what I'm currently listening to</p>
       <div className="flex justify-center items-center gap-2 mt-1">
-        <FaSpotify className="text-green-500" />
         <p>No listening to anything</p>
       </div>
     </footer>
   );
 };
-
 export default Footer;
