efaa29f3f54dcfc944c8722c0d98c3d304081fac
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-03-11 19:38:36 +0700

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,11 @@
-
 import React from "react";
-import { FaSpotify } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="text-center mt-6 text-sm text-gray-400">
       <hr className="h-px bg-gray-800 my-4 border-0" />
-      <p>See what I'm currently listening on Spotify</p>
       <div className="flex justify-center items-center gap-2 mt-1">
-        <FaSpotify className="text-green-500" />
-        <p>No listening to anything</p>
+      <p>© 2025 Faykal. <a href="https://whatsapp.com/channel/0029Vax4ADpI1rcZh3aveq1e">Privacy Policy</a></p>
       </div>
     </footer>
   );
