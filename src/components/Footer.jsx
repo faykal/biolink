3f745b5816d678f11027546e9de350569206ee90
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-03-11 18:40:28 +0700

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
-import React from "react"
-import { PiStarFourLight } from "react-icons/pi"
+
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="text-center mt-6 text-sm text-gray-400">
-            <hr className="h-px bg-gray-800 my-4 border-0"/>
-            <div className="flex justify-center items-center gap-2 mt-1">
-                <PiStarFourLight className="text-gray-500"/>
-                <p>© 2023 faykal.</p>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="text-center mt-6 text-sm text-gray-400">
+      <hr className="h-px bg-gray-800 my-4 border-0" />
+      <p><Faykal></Faykal></p>
+      <div className="flex justify-center items-center gap-2 mt-1">
+        <p>No listening to anything</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
